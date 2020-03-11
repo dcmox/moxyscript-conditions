bfe0a2b9761b7a4fa5352d1aa7ed22e1bedb3e24
dcmox <dancmox@comcast.net>
2020-03-11 18:07:02 -0500

    minor updates

--- moxy-conditions.ts
+++ moxy-conditions.ts
@@ -1,32 +1,35 @@
 const fs = require('fs')
+const _f = require('underscore-functions')
 
 export interface ICondition {
-    key: string,
-    operator: string,
-    value: Date | string | number | boolean,
-    valueB?: Date | string | number | boolean,
+	key: string
+	operator: string
+	value: Date | string | number | boolean
+	valueB?: Date | string | number | boolean
 }
 
-export interface IKeyValueObject { [key: string]: any }
+export interface IKeyValueObject {
+	[key: string]: any
+}
 
 export interface IValidationResult {
-    index: number,
-    reason?: string,
-    valid: boolean,
+	index: number
+	reason?: string
+	valid: boolean
 }
 
 export const opMap: IKeyValueObject = {
-    eq: '===',
-    gt: '>',
-    gte: '>=',
-    lt: '<',
-    lte: '<=',
-    neq: '!==',
+	eq: '===',
+	gt: '>',
+	gte: '>=',
+	lt: '<',
+	lte: '<=',
+	neq: '!==',
 }
 
 export const funcMap = {
-    between: true,
-    len: 'length',
+	between: true,
+	len: 'length',
 }
 
 export const opKeys = Object.keys(opMap)
@@ -39,150 +42,231 @@ export const REGEX_STRIP_UNSAFE_KEYS = /[^A-Z_a-z.-]/g
     * age eq value and residence eq house and registered between date1, date2
 */
 
-export const conditionsValidate = (conditions: ICondition[]): boolean | IValidationResult[] => {
-    const ret: IValidationResult[] = []
-    let pass: boolean = true
-    conditions.forEach((cond: ICondition, index: number) => {
-        let { key, operator, value, valueB } = cond
-        const keyReplaced = key.replace(REGEX_STRIP_UNSAFE_KEYS, '')
-        operator = operator.toLowerCase()
-        if (keyReplaced.length !== key.length) {
-            pass = false
-            ret.push({index, reason: 'Key contains unsafe character', valid: false})
-            return
-        }
-        if (typeof value === 'string' && !value.indexOf('`')) {
-            pass = false
-            ret.push({index, reason: 'Value contains unsafe character', valid: false})
-            return
-        }
-        // tslint:disable no-bitwise
-        if (~opKeys.indexOf(operator)) {
-            if (typeof value === 'object') { // Date support
-                if (!value.hasOwnProperty('valueOf')) {
-                    pass = false
-                    ret.push({index, reason: 'Value can only be of type Date | string',
-                        valid: false})
-                    return
-                }
-            } else {
-                if (~['string', 'boolean'].indexOf(typeof value) && !~['eq', 'neq'].indexOf(operator)) {
-                    pass = false
-                    ret.push({index, reason: `${typeof value} comparisons must only be eq | neq`, valid: false})
-                    return
-                }
-            }
-        } else if (~funcKeys.indexOf(operator)) {
-            if (operator === 'between' && valueB) {
-                if (typeof value !== typeof valueB) {
-                    pass = false
-                    ret.push({index, reason: 'Both values must be of same type', valid: false})
-                    return
-                }
-                if (~['string', 'boolean'].indexOf(typeof value) || ~['string', 'boolean'].indexOf(typeof valueB) ) {
-                    pass = false
-                    ret.push({index, reason: 'Between operator can only be used on Dates | numbers', valid: false})
-                    return
-                }
-                if (typeof value === 'object') { // Date support
-                    if (!value.hasOwnProperty('valueOf')) {
-                        pass = false
-                        ret.push({index, reason: 'Value can only be of type Date | string', valid: false})
-                        return
-                    }
-                }
-            }
-        } else {
-            pass = false
-            ret.push({index, reason: 'Operator is not valid.', valid: false})
-            return
-        }
-        ret.push({index, valid: true})
-    })
-    return pass ? true : ret
+export const conditionsValidate = (
+	conditions: ICondition[],
+): boolean | IValidationResult[] => {
+	const ret: IValidationResult[] = []
+	let pass: boolean = true
+	conditions.forEach((cond: ICondition, index: number) => {
+		let { key, operator, value, valueB } = cond
+		const keyReplaced = key.replace(REGEX_STRIP_UNSAFE_KEYS, '')
+		operator = operator.toLowerCase()
+		if (keyReplaced.length !== key.length) {
+			pass = false
+			ret.push({
+				index,
+				reason: 'Key contains unsafe character',
+				valid: false,
+			})
+			return
+		}
+		if (typeof value === 'string' && !value.indexOf('`')) {
+			pass = false
+			ret.push({
+				index,
+				reason: 'Value contains unsafe character',
+				valid: false,
+			})
+			return
+		}
+		// tslint:disable no-bitwise
+		if (~opKeys.indexOf(operator)) {
+			if (typeof value === 'object') {
+				// Date support
+				if (!value.hasOwnProperty('valueOf')) {
+					pass = false
+					ret.push({
+						index,
+						reason: 'Value can only be of type Date | string',
+						valid: false,
+					})
+					return
+				}
+			} else {
+				if (
+					~['string', 'boolean'].indexOf(typeof value) &&
+					!~['eq', 'neq'].indexOf(operator)
+				) {
+					pass = false
+					ret.push({
+						index,
+						reason: `${typeof value} comparisons must only be eq | neq`,
+						valid: false,
+					})
+					return
+				}
+			}
+		} else if (~funcKeys.indexOf(operator)) {
+			if (operator === 'between' && valueB) {
+				if (typeof value !== typeof valueB) {
+					pass = false
+					ret.push({
+						index,
+						reason: 'Both values must be of same type',
+						valid: false,
+					})
+					return
+				}
+				if (
+					~['string', 'boolean'].indexOf(typeof value) ||
+					~['string', 'boolean'].indexOf(typeof valueB)
+				) {
+					pass = false
+					ret.push({
+						index,
+						reason:
+							'Between operator can only be used on Dates | numbers',
+						valid: false,
+					})
+					return
+				}
+				if (typeof value === 'object') {
+					// Date support
+					if (!value.hasOwnProperty('valueOf')) {
+						pass = false
+						ret.push({
+							index,
+							reason: 'Value can only be of type Date | string',
+							valid: false,
+						})
+						return
+					}
+				}
+			}
+		} else {
+			pass = false
+			ret.push({ index, reason: 'Operator is not valid.', valid: false })
+			return
+		}
+		ret.push({ index, valid: true })
+	})
+	return pass ? true : ret
 }
 
 export const conditionsToString = (conditions: ICondition[]): string => {
-    let evalString = ''
-    conditions.forEach((cond: ICondition) => {
-        let { key, operator, value, valueB } = cond
-        key = key.replace(REGEX_STRIP_UNSAFE_KEYS, '')
-        if (typeof value === 'string') { value = '`' + value.replace(/`/g, '') + '`' } // Prevents Injections
-        // tslint:disable no-bitwise
-        if (~opKeys.indexOf(operator)) {
-            if (typeof value === 'object') { // Date support
-                evalString += `(new Date(item.${key}.valueOf()) ${opMap[operator]} ${value.valueOf()}) && `
-            } else {
-                evalString += `(item.${key} ${opMap[operator]} ${value}) && `
-            }
-        } else if (~funcKeys.indexOf(operator)) {
-            if (operator === 'between' && valueB) {
-                if (typeof value === 'object') { // Date support
-                    evalString += `(new Date(item.${key}.valueOf()) >= ${value.valueOf()} && new Date(item.${key}.valueOf()) <= ${valueB.valueOf()}) && `
-                } else {
-                    evalString += `(item.${key} >= ${value} && item.${key} <= ${valueB}) && `
-                }
-            }
-        }
-    })
-    return evalString.slice(0, evalString.length - 4)
+	let evalString = ''
+	conditions.forEach((cond: ICondition) => {
+		let { key, operator, value, valueB } = cond
+		key = key.replace(REGEX_STRIP_UNSAFE_KEYS, '')
+		if (typeof value === 'string') {
+			value = '`' + value.replace(/`/g, '') + '`'
+		} // Prevents Injections
+		// tslint:disable no-bitwise
+		if (~opKeys.indexOf(operator)) {
+			if (typeof value === 'object') {
+				// Date support
+				evalString += `(new Date(item.${key}.valueOf()) ${
+					opMap[operator]
+				} ${value.valueOf()}) && `
+			} else {
+				evalString += `(item.${key} ${opMap[operator]} ${value}) && `
+			}
+		} else if (~funcKeys.indexOf(operator)) {
+			if (operator === 'between' && valueB) {
+				if (typeof value === 'object') {
+					// Date support
+					evalString += `(new Date(item.${key}.valueOf()) >= ${value.valueOf()} && new Date(item.${key}.valueOf()) <= ${valueB.valueOf()}) && `
+				} else {
+					evalString += `(item.${key} >= ${value} && item.${key} <= ${valueB}) && `
+				}
+			}
+		}
+	})
+	return evalString.slice(0, evalString.length - 4)
 }
 
-export const itemsMatchConditions = (items: IKeyValueObject[], conditions: string) => {
-    // tslint:disable-next-line: no-eval
-    return items.filter((item: IKeyValueObject) => eval(conditions) )
+export const itemsMatchConditions = (
+	items: IKeyValueObject[],
+	conditions: string,
+) => {
+	// tslint:disable-next-line: no-eval
+	return items.filter((item: IKeyValueObject) => eval(conditions))
 }
 
-export const itemsFailConditions = (items: IKeyValueObject[], conditions: string) => {
-    // tslint:disable-next-line: no-eval
-    return items.filter((item: IKeyValueObject) => !(eval(conditions)) )
+export const itemsFailConditions = (
+	items: IKeyValueObject[],
+	conditions: string,
+) => {
+	// tslint:disable-next-line: no-eval
+	return items.filter((item: IKeyValueObject) => eval(conditions))
 }
 
 export class ConditionEditor {
-    public exportPath: string
-    private _condMap: IKeyValueObject
-    constructor(exportPath?: string) {
-        this.exportPath = exportPath || ''
-        this._condMap = {}
-        if (this.exportPath) { this.loadConditions(this.exportPath) }
-    }
+	public exportPath: string
+	private _condMap: IKeyValueObject
+	constructor(exportPath?: string) {
+		this.exportPath = exportPath || ''
+		this._condMap = {}
+		if (this.exportPath) {
+			this.loadConditions(this.exportPath)
+		}
+	}
 
-    public addCondition(name: string, conditions: ICondition[]): boolean | IValidationResult[] {
-        if (this._condMap[name]) { return false }
-        const validationResult = conditionsValidate(conditions)
-        if (validationResult) {
-            this._condMap[name] = conditionsToString(conditions)
-        } else {
-            return validationResult
-        }
-        return true
-    }
+	public addCondition(
+		name: string,
+		conditions: ICondition[],
+	): boolean | IValidationResult[] {
+		if (this._condMap[name]) {
+			return false
+		}
+		const validationResult = conditionsValidate(conditions)
+		if (validationResult) {
+			this._condMap[name] = conditionsToString(conditions)
+		} else {
+			return validationResult
+		}
+		return true
+	}
 
-    public getCondition(name: string): string | boolean { return this._condMap[name] ? this._condMap[name] : false }
+	public getCondition(name: string): string | boolean {
+		return this._condMap[name] ? this._condMap[name] : false
+	}
 
-    public runMatchCondition(name: string, data: any): IKeyValueObject[] | boolean {
-        return this._condMap[name] ? itemsMatchConditions(data, this._condMap[name]) : false
-    }
+	public runMatchCondition(
+		name: string,
+		data: any,
+	): IKeyValueObject[] | boolean {
+		return this._condMap[name]
+			? itemsMatchConditions(data, this._condMap[name])
+			: false
+	}
 
-    public runFailCondition(name: string, data: any): IKeyValueObject[] | boolean {
-        return this._condMap[name] ? itemsFailConditions(data, this._condMap[name]) : false
-    }
+	public runFailCondition(
+		name: string,
+		data: any,
+	): IKeyValueObject[] | boolean {
+		return this._condMap[name]
+			? itemsFailConditions(data, this._condMap[name])
+			: false
+	}
 
-    public saveConditions(): boolean {
-        if (this.exportPath === '' || this.exportPath.indexOf('.') === -1) { return false }
-        fs.writeFile(this.exportPath, JSON.stringify(this._condMap, null, 2), (err: any) => {
-            if (err) { throw new Error(err) }
-        })
-        return true
-    }
+	public saveConditions(): boolean {
+		if (this.exportPath === '' || this.exportPath.indexOf('.') === -1) {
+			return false
+		}
+		fs.writeFile(
+			this.exportPath,
+			JSON.stringify(this._condMap, null, 2),
+			(err: any) => {
+				if (err) {
+					throw new Error(err)
+				}
+			},
+		)
+		return true
+	}
 
-    public toJSON(prettify: boolean = false): any {
-        return prettify ? JSON.stringify(this._condMap, null, 2) : JSON.stringify(this._condMap)
-    }
+	public toJSON(prettify: boolean = false): any {
+		return prettify
+			? JSON.stringify(this._condMap, null, 2)
+			: JSON.stringify(this._condMap)
+	}
 
-    public loadConditions(path: string): boolean {
-        if (!fs.existsSync(path)) { return false }
-        this._condMap = JSON.parse(fs.readFileSync(path).toString())
-        return true
-    }
+	public loadConditions(path: string): boolean {
+		if (!fs.existsSync(path)) {
+			return false
+		}
+		this._condMap = JSON.parse(fs.readFileSync(path).toString())
+		return true
+	}
 }
--- tests/moxy-conditions.test.ts
+++ tests/moxy-conditions.test.ts
@@ -4,144 +4,183 @@ import * as testSuite from '../moxy-conditions'
 import { demographics } from './test-data'
 
 describe('Moxy Conditions test suite', () => {
-    it('Should not console.log (Sanitize Input)', () => {
-        const conditions = [
-            { // Injection Example
-                key: 'country',
-                operator: 'eq',
-                value: 'US\') && console.error(\'***unsanitized input***\') && (\'1\' === \'1',
-            },
-        ]
-        const spyMockLog = sinon.spy(console, 'error')
-        const conditionsForOffer = testSuite.conditionsToString(conditions)
-        testSuite.itemsMatchConditions(demographics.users, conditionsForOffer)
-        sinon.assert.notCalled(spyMockLog)
-        spyMockLog.restore()
-    })
+	it('Should not console.log (Sanitize Input)', () => {
+		const conditions = [
+			{
+				// Injection Example
+				key: 'country',
+				operator: 'eq',
+				value:
+					"US') && console.error('***unsanitized input***') && ('1' === '1",
+			},
+		]
+		const spyMockLog = sinon.spy(console, 'error')
+		const conditionsForOffer = testSuite.conditionsToString(conditions)
+		testSuite.itemsMatchConditions(demographics.users, conditionsForOffer)
+		sinon.assert.notCalled(spyMockLog)
+		spyMockLog.restore()
+	})
 
-    it('Conditions less than and equal to should match case.', () => {
-        const conditions = [
-            {
-                key: 'age',
-                operator: 'lt',
-                value: 20,
-            },
-            {
-                key: 'alcohol_use',
-                operator: 'eq',
-                value: true,
-            },
-        ]
-        const conditionsForOffer = testSuite.conditionsToString(conditions)
-        const result = testSuite.itemsMatchConditions(demographics.users, conditionsForOffer)
-        assert.equal(result.length, 1, 'Only one item should match')
-        assert.equal(result[0].id, 1937125, 'ID does not match!')
-    })
+	it('Conditions less than and equal to should match case.', () => {
+		const conditions = [
+			{
+				key: 'age',
+				operator: 'lt',
+				value: 20,
+			},
+			{
+				key: 'alcohol_use',
+				operator: 'eq',
+				value: true,
+			},
+		]
+		const conditionsForOffer = testSuite.conditionsToString(conditions)
+		const result = testSuite.itemsMatchConditions(
+			demographics.users,
+			conditionsForOffer,
+		)
+		assert.equal(result.length, 1, 'Only one item should match')
+		assert.equal(result[0].id, 1937125, 'ID does not match!')
+	})
 
-    it('Should test conditions gt and between and match case while two should fail.', () => {
-        const conditions = [
-            {
-                key: 'registered',
-                operator: 'gt',
-                value: new Date('2019-10-05'),
-            },
-            {
-                key: 'registered',
-                operator: 'between',
-                value: new Date('2019-10-07'),
-                valueB: new Date('2019-10-10'),
-            },
-        ]
-        const conditionsForOffer = testSuite.conditionsToString(conditions)
-        let result = testSuite.itemsMatchConditions(demographics.users, conditionsForOffer)
-        assert.equal(result.length, 1, 'Only one item should match')
-        assert.equal(result[0].id, 1937126, 'ID does not match!')
+	it('Should test conditions gt and between and match case while two should fail.', () => {
+		const conditions = [
+			{
+				key: 'registered',
+				operator: 'gt',
+				value: new Date('2019-10-05'),
+			},
+			{
+				key: 'registered',
+				operator: 'between',
+				value: new Date('2019-10-07'),
+				valueB: new Date('2019-10-10'),
+			},
+		]
+		const conditionsForOffer = testSuite.conditionsToString(conditions)
+		let result = testSuite.itemsMatchConditions(
+			demographics.users,
+			conditionsForOffer,
+		)
+		assert.equal(result.length, 1, 'Only one item should match')
+		assert.equal(result[0].id, 1937126, 'ID does not match!')
 
-        result = testSuite.itemsFailConditions(demographics.users, conditionsForOffer)
-        assert.equal(result.length, 2, 'Two items should fail conditions')
-    })
-    it('Should detect invalid key', () => {
-        const conditions = [
-            {
-                key: '`console.log(\'test\') && (1 ',
-                operator: 'eq',
-                value: 1,
-            },
-        ]
-        const result = testSuite.conditionsValidate(conditions)
-        if (typeof result !== 'boolean') {
-            assert.equal(result[0].reason, 'Key contains unsafe character')
-        }
-        assert.notEqual(result, true, 'Passing invalid key should not evaluate to true')
-    })
-    it('Should detect invalid value', () => {
-        const conditions = [
-            {
-                key: 'age',
-                operator: 'eq',
-                value: '`console.log(\'test\')`',
-            },
-        ]
-        const result = testSuite.conditionsValidate(conditions)
-        if (typeof result !== 'boolean') {
-            assert.equal(result[0].reason, 'Value contains unsafe character')
-        }
-        assert.notEqual(result, true, 'Passing invalid value should not evaluate to true')
-    })
-    it('Should detect invalid operators and invalid date comparisons', () => {
-        const conditions = [
-            {
-                key: 'registered',
-                operator: 'blah',
-                value: new Date('2019-10-05'),
-            },
-            {
-                key: 'registered',
-                operator: 'between',
-                value: new Date('2019-10-05'),
-                valueB: 'blah',
-            },
-            {
-                key: 'registered',
-                operator: 'between',
-                value: 'blah',
-                valueB: 'blah2',
-            },
-        ]
-        const result = testSuite.conditionsValidate(conditions)
-        if (typeof result !== 'boolean') {
-            assert.equal(result[0].reason, 'Operator is not valid.')
-            assert.equal(result[1].reason, 'Both values must be of same type')
-            assert.equal(result[2].reason, 'Between operator can only be used on Dates | numbers')
-        }
-        assert.notEqual(result, true, 'Passing invalid operator should not evaluate to true')
-    })
-    it('Should detect invalid string and string comparisons', () => {
-        const conditions = [
-            {
-                key: 'age',
-                operator: 'gt',
-                value: true,
-            },
-            {
-                key: 'age',
-                operator: 'lt',
-                value: true,
-            },
-            {
-                key: 'age',
-                operator: 'gt',
-                value: 'blah',
-            },
-        ]
-        const result = testSuite.conditionsValidate(conditions)
-        if (typeof result !== 'boolean') {
-            assert.equal(result[0].reason, 'boolean comparisons must only be eq | neq')
-            assert.equal(result[1].reason, 'boolean comparisons must only be eq | neq')
-            assert.equal(result[2].reason, 'string comparisons must only be eq | neq')
-        }
-        assert.notEqual(result, true, 'Passing invalid operator should not evaluate to true')
-    })
+		result = testSuite.itemsFailConditions(
+			demographics.users,
+			conditionsForOffer,
+		)
+		assert.equal(result.length, 2, 'Two items should fail conditions')
+	})
+	it('Should detect invalid key', () => {
+		const conditions = [
+			{
+				key: "`console.log('test') && (1 ",
+				operator: 'eq',
+				value: 1,
+			},
+		]
+		const result = testSuite.conditionsValidate(conditions)
+		if (typeof result !== 'boolean') {
+			assert.equal(result[0].reason, 'Key contains unsafe character')
+		}
+		assert.notEqual(
+			result,
+			true,
+			'Passing invalid key should not evaluate to true',
+		)
+	})
+	it('Should detect invalid value', () => {
+		const conditions = [
+			{
+				key: 'age',
+				operator: 'eq',
+				value: "`console.log('test')`",
+			},
+		]
+		const result = testSuite.conditionsValidate(conditions)
+		if (typeof result !== 'boolean') {
+			assert.equal(result[0].reason, 'Value contains unsafe character')
+		}
+		assert.notEqual(
+			result,
+			true,
+			'Passing invalid value should not evaluate to true',
+		)
+	})
+	it('Should detect invalid operators and invalid date comparisons', () => {
+		const conditions = [
+			{
+				key: 'registered',
+				operator: 'blah',
+				value: new Date('2019-10-05'),
+			},
+			{
+				key: 'registered',
+				operator: 'between',
+				value: new Date('2019-10-05'),
+				valueB: 'blah',
+			},
+			{
+				key: 'registered',
+				operator: 'between',
+				value: 'blah',
+				valueB: 'blah2',
+			},
+		]
+		const result = testSuite.conditionsValidate(conditions)
+		if (typeof result !== 'boolean') {
+			assert.equal(result[0].reason, 'Operator is not valid.')
+			assert.equal(result[1].reason, 'Both values must be of same type')
+			assert.equal(
+				result[2].reason,
+				'Between operator can only be used on Dates | numbers',
+			)
+		}
+		assert.notEqual(
+			result,
+			true,
+			'Passing invalid operator should not evaluate to true',
+		)
+	})
+	it('Should detect invalid string and string comparisons', () => {
+		const conditions = [
+			{
+				key: 'age',
+				operator: 'gt',
+				value: true,
+			},
+			{
+				key: 'age',
+				operator: 'lt',
+				value: true,
+			},
+			{
+				key: 'age',
+				operator: 'gt',
+				value: 'blah',
+			},
+		]
+		const result = testSuite.conditionsValidate(conditions)
+		if (typeof result !== 'boolean') {
+			assert.equal(
+				result[0].reason,
+				'boolean comparisons must only be eq | neq',
+			)
+			assert.equal(
+				result[1].reason,
+				'boolean comparisons must only be eq | neq',
+			)
+			assert.equal(
+				result[2].reason,
+				'string comparisons must only be eq | neq',
+			)
+		}
+		assert.notEqual(
+			result,
+			true,
+			'Passing invalid operator should not evaluate to true',
+		)
+	})
 })
 
 // {
